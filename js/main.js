7a0952de39fba5c6f77e534ce160d7526ae11a01
th1s1sau <110425101+th1s1sau@users.noreply.github.com>
2022-08-02 11:10:38 -0500

    added stuff
    * setting up proximity chat guide
    * light/dark mode toggle
    * header
    * basic styles

---
+++ js/main.js
@@ -0,0 +1,30 @@
+function toggleLightMode() {
+    var element = document.body
+    element.classList.toggle("light-mode");
+}
+
+var website_name = 'http://th1s1sau.github.io'
+
+class Header extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+        <table class="header-content"><tr>
+            <td class="home-link"><b><a href="${website_name}/index.html">th1s1sau</a></b></td>
+            <td class="light-dark"><a href="#" onclick="parent.toggleLightMode()">Light/Dark</a></td>
+        </tr></table>
+      `;
+    }
+  }
+
+function setHTML(className, displayValue) {
+    var items = document.getElementsByClassName(className);
+    for (var i=0; i < items.length; i++) {
+        items[i].innerHTML = displayValue;
+    }
+  }
+
+function idHTML(idName, displayValue) {
+    document.getElementById(idName).innerHTML = displayValue;
+}
+
+customElements.define('main-header', Header);
